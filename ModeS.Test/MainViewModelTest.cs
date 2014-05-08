7725ca932d9460477d49eb415b6af0036c6050d8
Yolkin Yura <r_yolka@mail.ru>
2014-05-08 12:48:00 +0300

    Added MainViewModelTest class for testing mainview page

---
+++ ModeS.Test/MainViewModelTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModeS.ViewModel;
+
+namespace ModeS.Test
+{
+    [TestClass]
+    public class MainViewModelTest
+    {
+        [TestMethod]
+        public void AircraftListTest()
+        {
+            var data = new DataLayerMock();
+            var aircraftList = data.GetAirCraftList();
+            var mainview = new MainView(data);
+            CollectionAssert.AreEqual(aircraftList, mainview.AirCrafts);
+        }
+
+        [TestMethod]
+        public void CountryListTest()
+        {
+            var data = new DataLayerMock();
+            var countries = data.GetCountriList();
+            var mainView = new MainView(data);
+            CollectionAssert.AreEqual(countries, mainView.Countries);
+        }
+
+        [TestMethod]
+        public void OperatorListTest()
+        {
+            var data = new DataLayerMock();
+            var oper = data.GetOperators();
+            var mainView = new MainView(data);
+            CollectionAssert.AreEqual(oper, mainView.Operators);
+        }
+    }
+}
